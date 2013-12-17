919705309435628e72aaf5d20ef3184f6b97962e
CelioBarros <celio.filho@ccc.ufcg.edu.br>
2013-12-16 23:12:12 -0200

    Organizando o commit inicial

---
+++ lab3/app/classes/Planejamento.java
@@ -0,0 +1,32 @@
+package classes;
+
+//CONTROLLER: controlador do sistema
+public class Planejamento { 
+
+	private Disciplina[] listaDisciplinas;
+
+	public Planejamento() {
+		listaDisciplinas = new Disciplina[6];
+	}
+
+	public Disciplina[] listaDeDisciplinas() {
+		return listaDisciplinas;
+	}
+
+	public int calculaTotalDeCreditos() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void adicionaDisciplinas(Disciplina disciplina) {
+		for (int i = 0; i < listaDisciplinas.length; i++) {
+			if (listaDisciplinas[i] == null) {
+				listaDisciplinas[i] = disciplina;
+				break;
+			}
+		}
+
+	}
+
+}
+
