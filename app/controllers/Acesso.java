278906ed774bf6053259877d817a3249d59776e5
BrendhaCruz <brendha.cruz@gmail.com>
2014-04-07 14:01:44 -0300

    alteracoes

--- app/controllers/Acesso.java
+++ app/controllers/Acesso.java
@@ -12,62 +12,59 @@ import com.avaje.ebean.Ebean;
 
 public class Acesso extends Controller {
 
-	public static class Login {
-
-	    public String email;
-	    public String senha;
-
-				
-	public String validate() {
-	    
-	    return null;
-	}
-}
+   public static class Login {
+       public String email;
+        public String senha;
+               
+    }
 	
 	public static class Cadastro {
 		private String email;
-		private String senha;
 		private String nome;
+		private String senha;
 		
 	}
-
+			
+	public static Result cadastro() {
+		return ok(views.html.cadastraUsuario.render(Form.form(Cadastro.class)));
+	}
 
 		public static Result login() {
 			return ok(views.html.logado.render(Form.form(Login.class)));
 		}
 
-		public static Result authenticate() {
-		    Form<Login> loginForm = form(Login.class).bindFromRequest();
-		   // if (loginForm.hasErrors()) {
-		   //    return badRequest(views.html.logado.render(loginForm));
-		    //} else {
-		        session().clear();
-		        session("email", loginForm.get().email);
-		        return redirect(routes.Application.index());
-		   // }
-		}
-
 		public static Result logout() {
 		    session().clear();
-		    flash("success", "You've been logged out");
+		    flash("success", "Você não está mais logado!");
 		    return redirect(routes.Acesso.login());
 		}	
 		
-		public static Result criaUsuario() {
-			Form<Cadastro> cadastroForm = Form.form(Cadastro.class).bindFromRequest();
-			
-			if (cadastroForm.hasErrors()) {
-				return badRequest(views.html.cadastraUsuario.render(cadastroForm));
+		public static Result authenticate() {
+			Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+			if (loginForm.hasErrors()) {
+				return badRequest(views.html.logado.render(loginForm));
 			} else {
-				Usuario novoAluno =	new Usuario();
-				novoAluno.setEmail(cadastroForm.get().email);
-				novoAluno.setSenha(cadastroForm.get().senha);
-				novoAluno.setNome(cadastroForm.get().nome);
-				Ebean.save(novoAluno);
-				return redirect(routes.Application.index());
+			session().clear();
+			Usuario user = Usuario.findEmail(loginForm.get().email);
+			if (user.getSistemaPlanejamento() == null) {
+				session().clear();
+			    flash("success", "Login Inválido!");
+			    return redirect(routes.Acesso.login());
 			}
+            session("email", loginForm.get().email);
+			session("sistema_planejamento_id", String.valueOf(user.getSistemaPlanejamento().getId()));
+            return redirect(routes.Application.index());
+        }
+    }
+		
+		public static Result criaUsuario() {
+			Form<Usuario> formUser = Form.form(Usuario.class).bindFromRequest();
+			Usuario us = new Usuario(formUser.bindFromRequest().get().getEmail(), formUser.bindFromRequest().get().getNome(), 
+	        formUser.bindFromRequest().get().getSenha());
+			us.save();
+			flash("sucesso", "cadastrou!");
+			return redirect(routes.Application.index());
 		}
-
 		
 		public static Result cadastraUsuario() {
 			return ok(views.html.cadastraUsuario.render(Form.form(Cadastro.class)));
@@ -84,12 +81,12 @@ public class Acesso extends Controller {
 					String nome = "User " + valor;
 					String senha = "senha" + valor;
 					
-					Usuario novoAluno =	new Usuario();
+					Usuario novoAluno =	new Usuario(nome, email, senha);
 					novoAluno.setEmail(email);
 					novoAluno.setNome(nome);
 					novoAluno.setSenha(senha);
 					
-					Ebean.save(novoAluno);
+					novoAluno.save();
 
 					valor ++;
 				}
